442bc87a70677e570b862c2dcb01078a49523e10
Dylan Frankland <dylan@frankland.io>
2017-03-12 12:06:41 -0700

    Add directory argument override

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const untildify = require('untildify');
 
+const workingDirectoryOverride = process.argv[1];
+
 const pull = value => value ? untildify(value) : process.env.HOME;
 const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
 
@@ -7,7 +9,7 @@ exports.middleware = store => next => action => {
   const { type } = action;
 
   if (type === 'CONFIG_LOAD' || type === 'CONFIG_RELOAD') {
-    push(pull(action.config.workingDirectory));
+    push(pull(workingDirectoryOverride || action.config.workingDirectory));
   }
 
   next(action);
