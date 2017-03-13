02c79ea513169bba601931ed7f836b3f2604dcf5
Dylan Frankland <dylan@frankland.io>
2017-03-12 17:04:25 -0700

    Get working directory isomorphically

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const untildify = require('untildify');
+const electron = require('electron');
 
-const workingDirectoryOverride = process.argv[1];
+let workingDirectoryOverride;
 
-const pull = value => value ? untildify(value) : process.env.HOME;
+if (
+  electron.remote &&
+  electron.remote.process &&
+  Array.isArray(electron.remote.process.argv)
+) {
+  workingDirectoryOverride = electron.remote.process.argv[1];
+} else if (
+  process &&
+  Array.isArray(process.argv)
+) {
+  workingDirectoryOverride = process.argv[1];
+}
+
+const pull = value => {
+  if (workingDirectoryOverride) return untildify(workingDirectoryOverride);
+  return value ? untildify(value) : process.env.HOME;
+};
+
 const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
 
 exports.middleware = store => next => action => {
-  const { type } = action;
+  const {
+    type,
+    config: {
+      workingDirectory,
+    } = {},
+  } = action;
 
   if (type === 'CONFIG_LOAD' || type === 'CONFIG_RELOAD') {
-    push(pull(workingDirectoryOverride || action.config.workingDirectory));
+    push(pull(workingDirectory));
   }
 
   next(action);
